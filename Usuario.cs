b3437655626c31b499b767ff414c655006e08249
Stacyrecinos-777 <rh0295032025@unab.edu.sv>
2026-04-19 21:12:35 -0600

    Usuarios

---
+++ Usuario.cs
@@ -0,0 +1,11 @@
+﻿namespace MiAgendaWeb.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string NombreUsuario { get; set; } = "";
+        public string Password { get; set; } = "";
+        public string Rol { get; set; } = "Estandar";
+        public bool EstaActivo { get; set; } = true;
+    }
+}
